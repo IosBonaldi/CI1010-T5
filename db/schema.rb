13840b9ba6986a6e1314310e4a2355d5ac901d2f
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-06-23 05:13:17 -0300

    [DEL]: Old schema tables

--- db/schema.rb
+++ db/schema.rb
@@ -49,26 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_225805) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "invoices", force: :cascade do |t|
-    t.integer "order_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_invoices_on_order_id"
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.string "customer"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.decimal "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -84,5 +64,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_225805) do
 
   add_foreign_key "cars", "brands"
   add_foreign_key "documents", "cars"
-  add_foreign_key "invoices", "orders"
 end
