dd79b12a16a2cb24c20b1330b12997d83670c1e3
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-06-22 21:58:12 -0300

    [ADD]: Bootstrap

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem "bootsnap", require: false
 
 gem "devise"
 
+gem "bootstrap"
+
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module T5
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.enabled = true
   end
 end
