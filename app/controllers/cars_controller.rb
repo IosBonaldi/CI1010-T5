6a0528bf1524e8bffd5c8cebd369f814618be60e
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-06-23 02:44:53 -0300

    [ADD] Relational data in show views

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -69,6 +69,6 @@ class CarsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def car_params
-    params.require(:car).permit(:brand_id, :model)
+    params.require(:car).permit(:brand_id, :model, :engine_ids => [])
   end
 end
